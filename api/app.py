1c8c69b49dcc85815058e8a792be254fabd88db0
Billy <ShklyeV.VoVa@yandex.ru>
2024-09-29 01:07:44 +0300

    uwu

--- api/app.py
+++ api/app.py
@@ -1,4 +1,3 @@
-import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
--- api/input.py
+++ api/input.py
@@ -1,6 +1,3 @@
-import json
-import psycopg2
-
 db_config = {
     'dbname': 'qablit79',
     'user': 'qablit79_user',
