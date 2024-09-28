32c536126472fb99bde6f21c24996cc2846cdee9
Billy <ShklyeV.VoVa@yandex.ru>
2024-09-29 01:08:46 +0300

    uwu

--- api/app.py
+++ api/app.py
@@ -3,14 +3,12 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def handler():
-
+def handle_post_1():
     response = {
         "message": "Alive"
     }
 
     return jsonify(response), 200
-
 
 if __name__ == '__main__':
     app.run(host='localhost', port=4000)
